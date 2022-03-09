2fc1b45e5b141be83eefad4f7a0794676c4f1858
BHIMRAJ YADAV <bhimrajyadav977@gmail.com>
2022-03-09 07:00:35 +0000

    Updated link

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Link from "next/link"
 import { Canvas } from "../components/Canvas";
 
 export default function Home() {
@@ -7,7 +8,7 @@ export default function Home() {
       <Head>
         <title>MNIST CLASSIFIER | ONNXJS | PYTORCH | Bhimraj Yadav</title>
         <link rel="icon" href="/favicon.ico" />
-       
+
       </Head>
       <header className="top-0 sticky left-0 w-full z-40 py-2 bg-gray-50 border-b border-gray-100">
         <div className="container mx-auto px-20 flex items-center justify-between h-16">
@@ -53,14 +54,20 @@ export default function Home() {
                 </a>
               </div>
               <div className="flex flex-col items-center mt-2 sm:mt-0 sm:flex-row sm:border-l sm:border-gray-300">
-                <svg
-                  className="w-6 h-6 mx-2"
-                  fill="black"
-                  viewBox="0 0 16 16"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path>
-                </svg>
+
+                <Link href="https://github.com/bhimrazy/nextjs-mnist-classifier-using-onnxjs">
+                  <a target="_blank" rel="noopener noreferrer">
+                    <svg
+                      className="w-6 h-6 mx-2"
+                      fill="black"
+                      viewBox="0 0 16 16"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path>
+                    </svg>
+                  </a>
+
+                </Link>
               </div>
             </nav>
           </div>
